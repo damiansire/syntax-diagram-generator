45dea7ee620b6d4fdb8da1eb845c030b9ea1e3ae
Damian Sire <damiansire@hotmail.com>
2023-09-12 05:48:00 -0300

    feat: add types for svg

---
+++ src/custom.d.ts
@@ -0,0 +1,8 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+        svg: React.SVGProps<SVGSVGElement>;
+        rect: React.SVGProps<SVGRectElement>;
+        text: React.SVGProps<SVGTextElement>;
+        // Otras definiciones de elementos JSX si es necesario
+    }
+}
